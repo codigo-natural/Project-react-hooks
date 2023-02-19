c6e7c2e02e55b08d1e22cba0d53590f6e1ea19c6
Code-Nature <codenature01001@gmail.com>
2023-02-19 12:16:58 -0500

    implementando useMemo

--- src/components/Characters/index.jsx
+++ src/components/Characters/index.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useState, useEffect, useReducer, useMemo } from 'react'
 
+// Initialize state to allow favorites to be tracked
 const initialState = {
   favorites: []
 }
 
+// Reducer used with useReducer to add/remove user favorites
 const favoriteReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_FAVORITE':
@@ -16,42 +18,76 @@ const favoriteReducer = (state, action) => {
   }
 }
 
+// Main component
 const Characters = () => {
+  // useState allows us to store and update data on a component level
   const [characters, setCharacters] = useState([]);
-  const [favorites, dispatvh] = useReducer(favoriteReducer, initialState);
+  // useReducer allows us to manage different states (favorites in this instance) and dispatch changes to that state with an action object
+  const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+  // useState to store the search value of a character
+  const [search, setSearch] = useState('');
 
+  // useEffect to make a request to the Rick and Morty API when the component mounts
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character/')
       .then(response => response.json())
       .then(data => setCharacters(data.results))
   }, []);
 
+  // use the dispatch function from useReducer to add a character to our favorites state
   const handleClick = favorite => {
-    dispatvh({ type: 'ADD_FAVORITE', payload: favorite });
+    dispatch({ type: 'ADD_FAVORITE', payload: favorite });
   }
+
+  // Update the search value with useState
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  // const filteredUsers = characters.filter((user) => {
+  //   return user.name.toLowerCase().includes(search.toLowerCase());
+  // })
+
+  // useMemo is used to optimize confusing code and improve performance of components
+  const filteredUsers = useMemo(() =>
+    characters.filter((user) => {
+      return user.name.toLowerCase().includes(search.toLowerCase());
+    }),
+    [characters, search]
+  )
 
   return (
     <div className='Characters'>
-
+      {/* loop through favorites in state and display them on screen */}
       {favorites.favorites.map(favorite => (
         <li key={favorite.id}>
           {favorite.name}
         </li>
       ))}
+      {/* search input to filter characters from the list  */}
+      <div className="Search">
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={handleSearch}
+        />
+      </div>
 
-      {characters.map(character => (
+
+      {filteredUsers.map(character => (
         <div className="item" key={character.id}>
           <h2>{character.name}</h2>
+          {/* click handler to add user favorites to state */}
           <button
             type='button'
             onClick={() => handleClick(character)}>
             Agregar a Favoritos
           </button>
-
         </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default Characters;
